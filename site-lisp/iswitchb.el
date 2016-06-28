4a68f298923ea9b5b546a2ab7fecaae56af2d735
Vijayendra Bapte <vijayendra.bapte@gmail.com>
2016-06-28 12:02:55 -0700

    added svn repo path

--- site-lisp/iswitchb.el
+++ site-lisp/iswitchb.el
@@ -22,6 +22,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
+;; **************
+;;  source checked out from http://cvs.savannah.gnu.org/viewvc/*checkout*/emacs/emacs/lisp/iswitchb.el
+;; **************
+
 ;;; Commentary:
 
 ;; Installation:
